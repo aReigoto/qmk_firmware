146cd034c65828853eb0eb867e05ccf0886b53c3
aReigoto <andrereigoto@gmail.com>
2022-12-25 11:29:21 +0000

    add mouse wheel

--- keyboards/crkbd/keymaps/arcCirque/keymap.c
+++ keyboards/crkbd/keymaps/arcCirque/keymap.c
@@ -50,11 +50,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_TRNS, KC_COMM,    KC_7,    KC_8,    KC_9, KC_BTN3,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END, KC_BTN3,  KC_DEL,
+      KC_TRNS, KC_COMM,    KC_7,    KC_8,    KC_9, KC_WH_U,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END, KC_BTN3,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS,  KC_DOT,    KC_4,    KC_5,    KC_6, KC_BTN2,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_BTN2,  OSL(6),
+      KC_TRNS,  KC_DOT,    KC_4,    KC_5,    KC_6, KC_WH_D,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_BTN2,  OSL(6),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS,    KC_0,    KC_1,    KC_2,    KC_3, KC_BTN1,                      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN1,   TO(0),
+      KC_TRNS,    KC_0,    KC_1,    KC_2,    KC_3,   KC_NO,                      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN1,   TO(0),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   MO(4), KC_TRNS
                                       //`--------------------------'  `--------------------------'
