5954149682657fece9bf0ce4dd4e1df7c4804c50
aReigoto <18371208+aReigoto@users.noreply.github.com>
2023-03-14 17:37:31 +0000

    first commit

---
+++ keyboards/crkbd/keymaps/arc/rules.mk
@@ -0,0 +1,8 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
